a6e5f834f64ec023f456d2c232ecdd4dda251872
Jawahar <mailjawa@gmail.com>
2026-03-09 16:26:36 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -22,6 +22,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
